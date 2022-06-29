6e30401c9e2fda9f73cf06a355a23ce29f92e865
Maria Kazim <kazim.maria@outlook.de>
2022-06-30 01:17:58 +0200

    add return statement to getAvailableMoves() and add parameters x and y to getAvaiableMovesDirections()

--- src/IsolationGame.java
+++ src/IsolationGame.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.util.ArrayList;
 
 public class IsolationGame {
@@ -10,18 +9,15 @@ public class IsolationGame {
     }
 
     public ArrayList<Move> getAvaiableMoves(int x, int y) {
-
         ArrayList<Move> availableMoves = new ArrayList<>();
-        activePlayer = FieldStatus[x][y];
-
-        availableMoves.addAll(getAvaiableMovesDirections(field, 0, 1));
-        availableMoves.addAll(getAvaiableMovesDirections(field, 0, -1));
-        availableMoves.addAll(getAvaiableMovesDirections(field, 1, 0));
-        availableMoves.addAll(getAvaiableMovesDirections(field, -1, 0));
-        availableMoves.addAll(getAvaiableMovesDirections(field, 1, 1));
-        availableMoves.addAll(getAvaiableMovesDirections(field, -1, -1));
-        availableMoves.addAll(getAvaiableMovesDirections(field, 1, -1));
-        availableMoves.addAll(getAvaiableMovesDirections(field, -1, 1));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, 0, 1));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, 0, -1));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, 1, 0));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, -1, 0));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, 1, 1));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, -1, -1));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, 1, -1));
+        availableMoves.addAll(getAvaiableMovesDirections(field, x, y, -1, 1));
 
 //        for (int i = 0; i <= field.length - 1; i++) {
 //            Move possibleMove = new Move(x, y, x, y+i);
@@ -58,25 +54,26 @@ public class IsolationGame {
 //            if (possibleMove.isValidMove(field)) availableMoves.add(possibleMove)
 //            else break;
 //        }
+        return availableMoves;
     }
 
-    private ArrayList<Move> getAvaiableMovesDirections(FieldStatus[][] field, int xMultiplier, int yMultiplier) {
+    private ArrayList<Move> getAvaiableMovesDirections(FieldStatus[][] field, int x, int y, int xMultiplier, int yMultiplier) {
         ArrayList<Move> moves = new ArrayList<>();
+
         for (int i = 0; i <= field.length - 1; i++) {
             Move possibleMove = new Move(x, y, x-i*xMultiplier, y-i*yMultiplier);
-            if (possibleMove.isValidMove(field)) moves.add(possibleMove)
+            if (possibleMove.isValidMove(field)) moves.add(possibleMove);
             else break;
         }
-        return
+        return moves;
     }
 }
 
 record Move(int x, int y, int destinationX, int destinationY) {
 
     public boolean isValidMove(FieldStatus[][] field) {
-        if (x >= 0 && x < field.length && y >= 0 && y < field.length && destinationX >= 0 && destinationX < field.length && destinationY >= 0 && destinationY < field.length) {
+        if (x >= 0 && x < field.length && y >= 0 && y < field.length && destinationX >= 0 && destinationX < field.length && destinationY >= 0 && destinationY < field.length)
             return false;
-        }
         FieldStatus player = field[x][y];
         // Hier eventuell dest nicht extra speichern
         FieldStatus dest = field[destinationX][destinationY];
