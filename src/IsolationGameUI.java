81a66f9e53a6356431f23c08e5ea29596740b6ad
Maria Kazim <kazim.maria@outlook.de>
2022-07-04 21:20:32 +0200

    finished gameOverScreen

--- src/IsolationGameUI.java
+++ src/IsolationGameUI.java
@@ -8,10 +8,11 @@ public class IsolationGameUI extends PApplet {
     private boolean greenTurn;
     private int currentFrame = 0;
     private boolean isValidMove;
+    private FieldState whoHasLost;
 
     public IsolationGameUI(boolean isDevVersion) {
         if (isDevVersion) {
-            currentState = GameState.GAME;
+            currentState = GameState.GAMEOVER;
         }
     }
 
@@ -56,10 +57,9 @@ public class IsolationGameUI extends PApplet {
         text("Don´t get stranded", width / 2, height - 550);
         fill(0, 95, 177);
         textSize(25);
-        text("Click to start", width / 2, height - 60);
+        text("Click to Start", width / 2, height - 60);
         textSize(15);
         text("Game Instructions", width / 2, height - 30);
-
     }
 
     public void gameInstructionScreen() {
@@ -67,7 +67,7 @@ public class IsolationGameUI extends PApplet {
         image(Resources.treasureMap, 10, height - 550, 200F * 2.6F, 166 * 2.8F);
         textFont(Resources.pirateFont);
         textAlign(CENTER);
-        fill(0);
+        fill(103, 40, 5);
         textSize(25);
         text("Game Instruction", width / 2, height - 460);
         textSize(18);
@@ -82,7 +82,6 @@ public class IsolationGameUI extends PApplet {
                 "Thus the goal of the game is\n" +
                 "to be the last player with a remaining move available", width / 2, height - 420);
         textFont(Resources.beteFont);
-        textAlign(CENTER);
         fill(0, 95, 177);
         textSize(25);
         text("Go Back", width / 2, height - 40);
@@ -93,26 +92,44 @@ public class IsolationGameUI extends PApplet {
         int xOffset = 90, yOffset = 171;
         background(Resources.islandScreen);
         image(Resources.treasureMap, 10, height - 550, 200F * 2.6F, 166F * 2.8F);
-        boolean isWhite = true;
         stroke(color(194, 145, 78));
+        boolean isGreenTurn = true;
         for (int x = 0; x < 350; x += 45) {
             for (int y = 0; y < 350; y += 45) {
-                if (isWhite)
+                if (isGreenTurn)
                     fill(color(194, 145, 78));
                 else
                     fill(color(229, 189, 128));
                 rect(x + xOffset, y + yOffset, 40, 40, 12);
-                isWhite = !isWhite;
+                isGreenTurn = !isGreenTurn;
             }
-            isWhite = !isWhite;
+            isGreenTurn = !isGreenTurn;
         }
-
         gameBoard.draw(this);
     }
 
     public void gameOverScreen() {
-        background(Resources.treasureMap);
+        background(Resources.treasureIslandScreen);
+        textFont(Resources.pirateFont);
+        textAlign(CENTER);
+        fill(252, 241, 201);
+        textSize(40);
+        text("Arrrggghhh\n" +
+                "Congrats Me Matey\n",width / 2, height - 550);
 
+        if (whoHasLost == FieldState.GREEN) {
+            image(Resources.redCrab, width/2-48, height-210, 90, 90);
+            textSize(30);
+            text("The Sweet Coffer is all Yours", width/2, height-220);
+        }
+        image(Resources.greenCrab, width/2-48, height-210, 90, 90);
+        textSize(30);
+        text("The Sweet Coffer is all Yours Yo Ho Ho", width/2, height-220);
+
+        textFont(Resources.beteFont);
+        fill(0, 95, 177);
+        textSize(25);
+        text("Play Again", width / 2, height - 40);
     }
 
     @Override
@@ -141,20 +158,18 @@ public class IsolationGameUI extends PApplet {
                     greenTurn = !greenTurn;
                 }
 
-                FieldState gameOver = gameBoard.whoLost();
-                if (gameOver != null) {
-                    if (gameOver == FieldState.GREEN || gameOver == FieldState.RED) {
+                whoHasLost = gameBoard.whoLost();
+                if (whoHasLost != null) {
+                    if (whoHasLost == FieldState.GREEN || whoHasLost == FieldState.RED) {
                         startGameOverScreen();
                     }
                 }
-
             }
             case GAMEOVER -> {
-
+                if (mouseX > 190 && mouseX < 353 && mouseY > 610 && mouseY < 630) {
+                    startInitScreen();
+                }
             }
-
-
-
         }
     }
 
@@ -173,6 +188,7 @@ public class IsolationGameUI extends PApplet {
     private void startGameOverScreen() {
         currentState = GameState.GAMEOVER;
     }
+
 
 
 
